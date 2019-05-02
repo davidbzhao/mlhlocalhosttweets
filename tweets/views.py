62bf40c46e562130e12b21ba90eb9e3860f5b94f
David Zhao <dz6hu@virginia.edu>
2019-05-02 11:42:24 -0400

    Hit Twitter API with twython

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,12 @@
-from django.http.response import HttpResponse
+from django.conf import settings
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
+
+from twython import Twython
 
 
 def index(request):
-    return HttpResponse('Hello world!')
+    twitter = Twython(settings.TWITTER_API_KEY, access_token=settings.TWITTER_ACCESS_TOKEN)
+    response = twitter.search(q='#mlhlocalhost')
+    return JsonResponse(response)
     
