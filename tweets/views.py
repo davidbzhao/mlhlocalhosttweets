3ffc4cf6799c238c424c02e272fdbc9e38112271
David Zhao <dz6hu@virginia.edu>
2019-05-04 08:00:54 -0400

    Display tweet json using a template

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from twython import Twython
@@ -8,5 +7,9 @@ from twython import Twython
 def index(request):
     twitter = Twython(settings.TWITTER_API_KEY, access_token=settings.TWITTER_ACCESS_TOKEN)
     response = twitter.search(q='#mlhlocalhost')
-    return JsonResponse(response)
+    tweets = response['statuses']
+    context = {
+        'tweets': tweets
+    }
+    return render(request, 'tweets/index.html', context)
     
